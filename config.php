262d1a67b27fcf68ebb02e05f170193ba8ff63cd
Pedro Ângelo <pangelo@void.io>
2015-05-23 14:18:06 +0100

    Add site config

--- config.php
+++ config.php
@@ -6,6 +6,17 @@ $config = array();
 // run the generator.php file or fill this with a long string
 // must not be empty
 $config['encryptionKey'] = '';
+$config['site_title']    = 'altLab Documenta'; // Site title
+$config['theme']         = 'altlab'; // Set the theme
+
+$config['plugins']['gibbs\\phileSubNavigation'] = array('active' => true);
+$config['plugins']['siezi\\phileServeContentFiles'] = ['active' => true];
+$config['plugins']['phile\\youtube'] = array('active' => true);
+
+$config['plugins']['pschmitt\\analytics'] = array('active' => true);
+
+$config['google_tracking_id'] = 'UA-20725619-1';
+
 
 // it is important to return the $config array!
 return $config;
